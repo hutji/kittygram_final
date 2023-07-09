57c166dc4d6888f1ed40b09f975f9f4a87f121fe
Pavel Lashkov <hutjicsgo@gmail.com>
2023-07-09 18:17:43 +0300

    last one

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.97.140', '127.0.0.1', 'localhost', 'hutjikitty.hopto.org']
+ALLOWED_HOSTS = ['51.250.97.140', '127.0.0.1', 'localhost', 'hutjikitty.zapto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
