1f132aa712619f3f041804375085745bdd17770f
Pavel Lashkov <hutjicsgo@gmail.com>
2023-07-18 22:49:49 +0300

    the newest version

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG_VALUE').lower() == 'true'
+DEBUG = os.getenv('DEBUG_VALUE','false').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,hutjikitty,zapto.org').split(',')
 
